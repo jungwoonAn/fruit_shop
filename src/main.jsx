039203b39d94923963bc955c0c96703ae0673a06
jungwoonAn <red0808@naver.com>
2026-02-01 13:16:40 +0900

    20260201_vuit_depoly

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import store from "./store";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/fruit_shop">
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </Provider>
